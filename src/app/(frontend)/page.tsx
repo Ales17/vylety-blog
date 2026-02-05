4dc4c2c8cbbec7871196119b35f4cd46cc0ade22
AlesBacul <alesbacul@gmail.com>
2026-02-05 09:58:47 +0100

    implement very optimistic basic richtext to jsx converting

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -4,56 +4,49 @@ import { getPayload } from 'payload'
 import React from 'react'
 import { fileURLToPath } from 'url'
 
+import { RichText, JSXConverters, LinkJSXConverter } from '@payloadcms/richtext-lexical/react'
+import type { SerializedEditorState } from '@payloadcms/richtext-lexical/lexical'
+
 import config from '@/payload.config'
 import './styles.css'
+import { DefaultTypedEditorState } from '@payloadcms/richtext-lexical'
+
+const headers = await getHeaders()
+const payloadConfig = await config
+const payload = await getPayload({ config: payloadConfig })
+const { user } = await payload.auth({ headers })
+
+async function getPosts() {
+  const result = await payload.find({
+    collection: 'posts', // required
+    page: 1,
+    limit: 10,
+    pagination: false, // If you want to disable pagination count, etc.
+    where: {}, // pass a `where` query here
+    sort: '-title',
+  })
+  return result
+}
 
 export default async function HomePage() {
-  const headers = await getHeaders()
-  const payloadConfig = await config
-  const payload = await getPayload({ config: payloadConfig })
-  const { user } = await payload.auth({ headers })
-
-  const fileURL = `vscode://file/${fileURLToPath(import.meta.url)}`
-
+  let posts = await getPosts()
+  console.log(posts)
+  let docs = posts.docs
   return (
-    <div className="home">
-      <div className="content">
-        <picture>
-          <source srcSet="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-favicon.svg" />
-          <Image
-            alt="Payload Logo"
-            height={65}
-            src="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-favicon.svg"
-            width={65}
-          />
-        </picture>
-        {!user && <h1>Welcome to your new project.</h1>}
-        {user && <h1>Welcome back, {user.email}</h1>}
-        <div className="links">
-          <a
-            className="admin"
-            href={payloadConfig.routes.admin}
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Go to admin panel
-          </a>
-          <a
-            className="docs"
-            href="https://payloadcms.com/docs"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Documentation
-          </a>
-        </div>
+    <>
+      <div>
+        {!docs
+          ? ''
+          : docs.map((obj) => {
+              const data = obj.content as DefaultTypedEditorState
+
+              return (
+                <div key={obj.id}>
+                  <RichText data={data} />{' '}
+                </div>
+              )
+            })}
       </div>
-      <div className="footer">
-        <p>Update this page by editing</p>
-        <a className="codeLink" href={fileURL}>
-          <code>app/(frontend)/page.tsx</code>
-        </a>
-      </div>
-    </div>
+    </>
   )
 }

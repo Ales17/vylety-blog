4a342cefe566159ed89ca96620496ce07cd9a36e
AlesBacul <alesbacul@gmail.com>
2026-02-05 11:09:08 +0100

    create default payload rich text component

---
+++ src/components/RichTextDefault.tsx
@@ -0,0 +1,36 @@
+import { Post } from '@/payload-types'
+import Link from 'next/link'
+import {
+  DefaultNodeTypes,
+  SerializedBlockNode,
+  SerializedLinkNode,
+  type DefaultTypedEditorState,
+} from '@payloadcms/richtext-lexical'
+
+import {
+  RichText,
+  JSXConverters,
+  LinkJSXConverter,
+  JSXConvertersFunction,
+} from '@payloadcms/richtext-lexical/react'
+
+type Props = {
+  data: DefaultTypedEditorState
+  enableGutter?: boolean
+  enableProse?: boolean
+} & React.HTMLAttributes<HTMLDivElement>
+
+type NodeTypes = DefaultNodeTypes
+
+const jsxConverters: JSXConvertersFunction<NodeTypes> = ({ defaultConverters }) => ({
+  ...defaultConverters,
+})
+
+export default function RichTextDefault(props: Props) {
+  const { data, ...rest } = props
+  return (
+    <>
+      <RichText converters={jsxConverters} data={data} />
+    </>
+  )
+}

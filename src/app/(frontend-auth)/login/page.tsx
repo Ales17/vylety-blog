c36cbd03ee0d401a6a8a8dc0f3be3142f5de5386
AlesBacul <alesbacul@gmail.com>
2026-02-05 18:27:43 +0100

    refactor login form to use Input component for email and password fields

--- src/app/(frontend-auth)/login/page.tsx
+++ src/app/(frontend-auth)/login/page.tsx
@@ -1,15 +1,12 @@
 import { login } from 'lib/fe-auth'
+import Input from '@/components/Input'
 
 export default function Login() {
   return (
     <div>
       <form action={login}>
-        <div>
-          <input type="email" name="email" id="" />
-        </div>
-        <div>
-          <input type="password" name="password" id="" />
-        </div>
+        <Input type={'email'} name={'email'} placeholder="email@email.cz" label="E-mail" />
+        <Input type="password" name="password" placeholder="******" label="Heslo" />
         <div>
           <input type="submit" value={'Přihlásit se'} />
         </div>

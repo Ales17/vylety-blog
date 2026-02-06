55be9a763bc2a71a7042ae49a83221e17f3d171c
AlesBacul <alesbacul@gmail.com>
2026-02-06 21:50:23 +0100

    add label prop to LinkButton component for displaying button text

--- src/components/LinkButton.tsx
+++ src/components/LinkButton.tsx
@@ -2,8 +2,13 @@ import Link from 'next/link'
 import type { Url } from 'next/dist/shared/lib/router/router'
 interface Props {
   href: Url
+  label: string
 }
 
-export default function LinkButton({ href }: Props) {
-  return <Link href={href} className="p-2 rounded text-white bg-blue-500"></Link>
+export default function LinkButton({ href, label }: Props) {
+  return (
+    <Link href={href} className="inline-block p-2 rounded text-white bg-blue-500">
+      {label}
+    </Link>
+  )
 }

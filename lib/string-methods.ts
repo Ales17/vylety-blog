cd3e3dacbbe86ee672e45d202f1fb82a3d47b470
AlesBacul <alesbacul@gmail.com>
2026-02-06 21:14:36 +0100

    add string utility functions for slugification and ID extraction from slugs

--- lib/string-methods.ts
+++ lib/string-methods.ts
@@ -6,3 +6,7 @@ export function slugify(title: string) {
     .normalize('NFD')
     .replace(/[\u0300-\u036f]/g, '')
 }
+
+export const getIdFromSlug = (slug: string): string | null => {
+  return slug.match(/^\d+/)?.[0] ?? null
+}

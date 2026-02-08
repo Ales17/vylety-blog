bb4d4d33fee3df2269b7a3680814ddb2c732e22b
AlesBacul <alesbacul@gmail.com>
2026-02-08 13:18:25 +0100

    refactor TripInfoItem component to support optional title and href props with conditional rendering

--- src/components/TripInfoItem.tsx
+++ src/components/TripInfoItem.tsx
@@ -1,12 +1,38 @@
+import { ExternalLinkIcon } from 'lucide-react'
 interface Props {
   icon: React.ReactNode
-  label: string
+  label: React.ReactNode
+  href?: string
+  title?: string
 }
 
-export default function TripInfoItem({ icon, label }: Props) {
+interface ConditionalWrapperProps {
+  children: React.ReactNode
+  href?: string
+}
+
+const ConditionalWrapper = ({ children, href }: ConditionalWrapperProps) => {
+  if (href) {
+    return (
+      <a target="_blank" rel="noopener nofollow noreferrer" href={href}>
+        {children}
+      </a>
+    )
+  } else {
+    return children
+  }
+}
+
+export default function TripInfoItem({ icon, label, title = '', href }: Props) {
+  let className = `${href ? 'hover:bg-slate-200' : ''} flex bg-slate-100 px-3 py-1.5 rounded-md gap-x-2 items-center transition-colors`
   return (
-    <li className="flex gap-x-1 bg-slate-100 rounded-md px-3 py-1">
-      {icon} {label}
+    <li title={title}>
+      <ConditionalWrapper href={href}>
+        <div className={className}>
+          {icon}
+          <span>{label}</span> {href && <ExternalLinkIcon size={16} />}
+        </div>
+      </ConditionalWrapper>
     </li>
   )
 }

d7f3bf984666b90fefc4f9df2d026e054531ce81
AlesBacul <alesbacul@gmail.com>
2026-02-08 10:43:51 +0100

    refactor Login component to add id attributes to Input fields for improved accessibility

--- src/app/(frontend-auth)/login/page.tsx
+++ src/app/(frontend-auth)/login/page.tsx
@@ -13,8 +13,14 @@ export default function Login() {
   return (
     <div>
       <form action={formAction}>
-        <Input type={'email'} name={'email'} placeholder="email@email.cz" label="E-mail" />
-        <Input type="password" name="password" placeholder="******" label="Heslo" />
+        <Input
+          type={'email'}
+          name={'email'}
+          id="email"
+          placeholder="email@email.cz"
+          label="E-mail"
+        />
+        <Input type="password" name="password" id="password" placeholder="******" label="Heslo" />
         <Button label={'Přihlásit se'} variant="primary" />
         <p aria-live="polite">{state?.message}</p>
       </form>

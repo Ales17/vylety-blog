df745805e602ebad518d6a645507b3402fc00c0d
AlesBacul <alesbacul@gmail.com>
2026-02-08 22:50:32 +0100

    refactor Nav component to enhance layout with max-width and backdrop blur

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -7,7 +7,7 @@ export default function Nav() {
   const navItemClass =
     'h-full w-full flex py-4 px-2 gap-x-2  hover:bg-slate-200 transition-colors justify-center rounded-4xl'
   return (
-    <div className=" m-2  rounded-4xl bg-slate-100  border-b border-slate-200">
+    <div className=" m-2 lg:mx-auto lg:max-w-4xl rounded-4xl bg-slate-100/60  border border-slate-200 backdrop-blur-lg">
       <div className="flex flex-col text-center">
         <header className="text-2xl p-2">Výlety</header>
         <nav>

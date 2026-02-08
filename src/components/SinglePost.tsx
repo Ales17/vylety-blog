eab617db1a9730526b6e4070423e824960890321
AlesBacul <alesbacul@gmail.com>
2026-02-08 13:19:35 +0100

    refactor SinglePost component to use TripInfoItem for improved layout and readability

--- src/components/SinglePost.tsx
+++ src/components/SinglePost.tsx
@@ -1,18 +1,45 @@
 import { Post } from '@/payload-types'
 import RichTextDefault from './RichTextDefault'
+import { MapIcon, Calendar1Icon, FootprintsIcon } from 'lucide-react'
+import TripInfoItem from './TripInfoItem'
 
 interface Props {
   post: Post
 }
+
+const formatDate = (date: any) => {
+  const dateParsed = Date.parse(date)
+  const formatter = new Intl.DateTimeFormat('cs-CZ', { dateStyle: 'long' })
+  return formatter.format(dateParsed)
+}
+
 export default function SinglePost({ post }: Props) {
   return (
     <div>
-      <div>{post.title}</div>
-      <div>{post.tripLength} km</div>
-      <div>Trasa: {post.mapsLink}</div>
-      <div>Datum: {post.eventDate}</div>
-      {!post.content ? '' : <RichTextDefault data={post.content} />}
-      {JSON.stringify(post)}
+      <h1>{post.title}</h1>
+
+      <ul className="flex flex-wrap justify-center gap-x-4 gap-y-2">
+        <TripInfoItem
+          title="Datum akce"
+          icon={<Calendar1Icon />}
+          label={formatDate(post.eventDate)}
+        />
+
+        <TripInfoItem
+          title="Délka trasy"
+          icon={<FootprintsIcon />}
+          label={`${post.tripLength} km`}
+        />
+
+        <TripInfoItem
+          icon={<MapIcon />}
+          label={'Mapa trasy'}
+          href={post.mapsLink}
+          title="Otevřít trasu"
+        />
+      </ul>
+
+      <div>{!post.content ? '' : <RichTextDefault data={post.content} />}</div>
     </div>
   )
 }

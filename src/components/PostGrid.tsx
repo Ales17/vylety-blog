04d23a10759f9e9fb8925500642fdb07921271c2
AlesBacul <alesbacul@gmail.com>
2026-02-09 12:43:14 +0100

    refactor PostGrid component layout for improved responsiveness and spacing

--- src/components/PostGrid.tsx
+++ src/components/PostGrid.tsx
@@ -14,16 +14,16 @@ export default function PostGrid(props: Props) {
   const docs = paginatedDocs.docs
 
   return (
-    <div className="flex flex-col gap-2">
+    <div className="grid gap-2 md:grid-cols-2">
       {docs.map((doc) => {
         const data = doc.content as DefaultTypedEditorState
         return (
           <div
             key={doc.id}
-            className="border rounded-3xl bg-white border-slate-200 p-2 md:p-4 flex flex-col gap-y-1"
+            className="border rounded-3xl bg-white border-slate-200 p-2 md:p-4 flex flex-col gap-2"
           >
             <h2 className="text-2xl">{doc.title}</h2>
-            <RichTextDefault className="line-clamp-2" data={data} />
+            <RichTextDefault className="line-clamp-2 flex-1" data={data} />
             <div>
               <LinkButton
                 href={`/posts/${doc.id}-${doc.slug}`}

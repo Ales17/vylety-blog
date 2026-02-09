215e5ec829a377a25d7575ad0259ab8981966dc1
AlesBacul <alesbacul@gmail.com>
2026-02-09 15:48:56 +0100

    style(LinkButton): adjust padding and gap for improved button appearance

--- src/components/LinkButton.tsx
+++ src/components/LinkButton.tsx
@@ -10,7 +10,7 @@ export default function LinkButton({ href, label, icon }: Props) {
   return (
     <Link
       href={href}
-      className="inline-flex gap-x-2 hover:gap-x-2.5 py-4 px-2 bg-slate-900 text-white rounded-2xl font-medium transition-all hover:bg-slate-800 active:scale-95 shadow-sm"
+      className="inline-flex gap-x-1.5 hover:gap-x-3 py-2 px-2 bg-slate-900 text-white rounded-2xl font-medium transition-all hover:bg-slate-800 active:scale-95 shadow-sm"
     >
       <span>{label}</span> {icon && <span>{icon}</span>}
     </Link>

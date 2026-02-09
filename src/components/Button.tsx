dda4a5141ffad59da6b65f7e46caddd499460918
AlesBacul <alesbacul@gmail.com>
2026-02-09 15:50:29 +0100

    style(Button): update primary button colors and enhance base design for improved appearance

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,12 +7,12 @@ interface Props extends React.DetailedHTMLProps<
 }
 
 let colors: Record<string, string> = {
-  primary: 'text-white bg-blue-500',
+  primary: 'bg-slate-900 text-white  hover:bg-slate-800 active:scale-95',
   secondary: 'text-white bg-amber-400',
 }
 export default function Button({ onClick, type, label, variant = 'primary' }: Props) {
   const activeVariantClass = colors[variant]
-  const baseDesignClasses = 'p-2 rounded'
+  const baseDesignClasses = 'p-2 rounded-2xl font-medium transition-all shadow-sm'
 
   return (
     <>

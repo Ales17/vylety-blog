378afe6b0ae6fa5162e6026c003614e7ff13e21b
AlesBacul <alesbacul@gmail.com>
2026-02-10 20:48:06 +0100

    style(layout): adjust max width of main section for improved layout

--- src/app/(fe)/(auth)/layout.tsx
+++ src/app/(fe)/(auth)/layout.tsx
@@ -15,7 +15,7 @@ export default async function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="cs">
       <body>
-        <main className="bg-white m-2 lg:mx-auto lg:max-w-2xl border rounded-3xl border-slate-200 p-2 md:p-4">
+        <main className="bg-white m-2 lg:mx-auto lg:max-w-xl border rounded-3xl border-slate-200 p-2 md:p-4">
           {children}
         </main>
       </body>

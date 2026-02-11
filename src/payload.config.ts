3ae6795a3c035ed02c7dcd97d7b860356c50103b
AlesBacul <alesbacul@gmail.com>
2026-02-11 13:45:03 +0100

    fix: update database connection string to use environment variables

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -28,7 +28,9 @@ export default buildConfig({
   },
   db: postgresAdapter({
     pool: {
-      connectionString: process.env.DATABASE_URL || '',
+      connectionString:
+        `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}` ||
+        '',
     },
     //push: true, // IN DEV
   }),
